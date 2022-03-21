10f9b3afa2cd3fa07164c56aac2e4c2c96e4252d
Niss333 <de_niss@list.ru>
2022-03-21 15:32:22 +0300

    sdk-0.9.2

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/mattermost/mattermost-plugin-starter-template
+module github.com/ITCDEK/mattermost-plugin-livekit
 
 go 1.16
 
 require (
-	github.com/livekit/protocol v0.11.14-0.20220223195254-d8c251e13231
-	github.com/livekit/server-sdk-go v0.9.1
+	github.com/livekit/protocol v0.12.0
+	github.com/livekit/server-sdk-go v0.9.2
 	github.com/mattermost/mattermost-server/v6 v6.2.1
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
--- server/plugin.go
+++ server/plugin.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/livekit/protocol/auth"
 	"github.com/livekit/protocol/livekit"
 	kitSDK "github.com/livekit/server-sdk-go"
 	"github.com/mattermost/mattermost-server/v6/model"
@@ -19,6 +22,7 @@ import (
 // https://github.com/niklabh/mattermost-plugin-webrtc-video
 // https://github.com/Kopano-dev/mattermost-plugin-kopanowebmeetings
 // https://github.com/blindsidenetworks/mattermost-plugin-bigbluebutton
+// https://developers.mattermost.com/integrate/plugins/server/reference/
 
 // LivePlugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type LivePlugin struct {
@@ -34,18 +38,18 @@ type LivePlugin struct {
 	master        *kitSDK.RoomServiceClient
 }
 
-func (kit *LivePlugin) OnActivate() error {
-	kit.API.LogInfo("Activating...")
-	config := kit.getConfiguration()
+func (kp *LivePlugin) OnActivate() error {
+	kp.API.LogInfo("Activating...")
+	config := kp.getConfiguration()
 	// validate config
-	kit.configuration = config
+	kp.configuration = config
 
-	command, err := kit.compileSlashCommand()
+	command, err := kp.compileSlashCommand()
 	if err != nil {
 		return err
 	}
 
-	if err = kit.API.RegisterCommand(command); err != nil {
+	if err = kp.API.RegisterCommand(command); err != nil {
 		return err
 	}
 
@@ -55,28 +59,65 @@ func (kit *LivePlugin) OnActivate() error {
 		Description: "A bot account created by the LiveKit plugin",
 	}
 
-	bot, ae := kit.API.CreateBot(liveBot)
+	bot, ae := kp.API.CreateBot(liveBot)
 	if ae == nil {
-		kit.bot = bot
+		kp.bot = bot
 	}
 
-	server := kit.configuration.Servers[0]
-	kit.master = kitSDK.NewRoomServiceClient(server.Host, server.ApiKey, server.ApiSecret)
+	server := kp.configuration.Servers[0]
+	kp.master = kitSDK.NewRoomServiceClient(server.Host, server.ApiKey, server.ApiSecret)
 
 	return nil
 }
 
-func (kit *LivePlugin) OnDeactivate() error {
+func (kp *LivePlugin) OnDeactivate() error {
 	return nil
 }
 
-func (kit *LivePlugin) newRoom(roomName string) error {
-	room, err := kit.master.CreateRoom(context.Background(), &livekit.CreateRoomRequest{Name: roomName})
-	fmt.Println(room.CreationTime)
+func getJoinToken(apiKey, apiSecret, room, identity string) (string, error) {
+	at := auth.NewAccessToken(apiKey, apiSecret)
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     room,
+	}
+	at.AddGrant(grant).
+		SetIdentity(identity).
+		SetValidFor(time.Hour)
+
+	return at.ToJWT()
+}
+
+func (kp *LivePlugin) newRoom(roomName, channelID, rootID, topic string) error {
+	room, err := kp.master.CreateRoom(context.Background(), &livekit.CreateRoomRequest{Name: roomName})
+	kp.API.LogInfo("room created at", room.CreationTime)
+	post := &model.Post{
+		UserId:    kp.bot.UserId,
+		ChannelId: channelID,
+		RootId:    rootID,
+		Message:   "I have started a meeting",
+		Type:      "custom_zoom",
+		Props: map[string]interface{}{
+			"room_capacity": room.MaxParticipants,
+			"room_name":     room.Name,
+			// "meeting_link":             meetingURL,
+			// "meeting_status":           zoom.WebhookStatusStarted,
+			// "meeting_personal":         false,
+			// "meeting_topic":            topic,
+			// "meeting_creator_username": creator.Username,
+			// "meeting_provider":         zoomProviderName,
+			// "attachments":              []*model.SlackAttachment{&slackAttachment},
+		},
+	}
+
+	newRoomPost, appErr := kp.API.CreatePost(post)
+	kp.API.LogInfo("room post created with ID =", newRoomPost.Id)
+	if appErr != nil {
+		return appErr
+	}
 	return err
 }
 
-func (kit *LivePlugin) compileSlashCommand() (*model.Command, error) {
+func (kp *LivePlugin) compileSlashCommand() (*model.Command, error) {
 	acData := model.NewAutocompleteData("call", "[command]", "Start a LiveKit meeting in current channel. Other available commands: start, help, settings")
 	start := model.NewAutocompleteData("start", "[topic]", "Start a new meeting in the current channel")
 	start.AddTextArgument("(optional) The topic of the new meeting", "[topic]", "")
@@ -94,21 +135,34 @@ func (kit *LivePlugin) compileSlashCommand() (*model.Command, error) {
 }
 
 // ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (kit *LivePlugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+func (kp *LivePlugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+	type roomRequest struct {
+		ChannelID string `json:"channel_id"`
+		Personal  bool   `json:"personal"`
+		Topic     string `json:"topic"`
+		MeetingID int    `json:"meeting_id"`
+	}
 	userID := r.Header.Get("Mattermost-User-ID")
 	if userID == "" {
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
 		return
 	}
-	switch r.URL.Path {
-	case "/webhook":
-		fmt.Fprint(w, "Hello, world! This hook is not implemented yet.")
-	case "/token":
-		fmt.Fprint(w, "Hello, world! Token feature is not implemented yet.")
-		// kit.API.LogInfo()
-	default:
-		http.NotFound(w, r)
+	var rr roomRequest
+	err := json.NewDecoder(r.Body).Decode(&rr)
+	if err == nil {
+		switch r.URL.Path {
+		case "/webhook":
+			fmt.Fprint(w, "Hello, world! This hook is not implemented yet.")
+		case "/room":
+			fmt.Fprint(w, "Hello, world! The new room feature is not implemented yet.")
+		case "/token":
+			fmt.Fprint(w, "Hello, world! Token feature is not implemented yet.")
+			// kit.API.LogInfo()
+		default:
+			http.NotFound(w, r)
+		}
 	}
+	http.Error(w, err.Error(), http.StatusBadRequest)
 }
 
 // See https://developers.mattermost.com/extend/plugins/server/reference/
